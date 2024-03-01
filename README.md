f1cced293f1831761ad773c214f08bfb225ee2b1
Mallory Howard <mallory1313@msn.com>
2024-02-29 22:28:45 -0700

    started javascript code. added a few items to README

---
+++ README.md
@@ -0,0 +1,15 @@
+## SVG Logo Maker
+
+# Installation
+Install Node.js, if needed.   
+
+Create a .gitignore file and include "node_modules", ".vscode", and "package-lock.json" in it, so that these files aren't tracked or uploaded to GitHub. Be sure to create your .gitignore file before installing any npm dependencies.   
+
+Make sure that your repo includes a package.json with the required dependencies. You can create one by running "npm init" when you first set up the project, before installing any dependencies.   
+
+This application requires the Inquirer.js module. To install inquirer, navigate to your terminal and enter "npm i inquirer@8.2.4".  
+
+This application requires the Jest.js module. To install jest, navigate to your terminal and enter "npm i --save-dev jest".  
+
+# Credits
+https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes | https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Texts | 
---
+++ index.js
@@ -0,0 +1,59 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle, Square, Triangle } = require('./lib/shapes.js');
+
+function createLogo() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: input => input.length <= 3 ? true : 'Text must be up to three characters.'
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color:',
+      validate: input => /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i.test(input) || /^[a-zA-Z]+$/.test(input) ? true : 'Enter a valid color keyword or hexadecimal number.'
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color:',
+      validate: input => /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i.test(input) || /^[a-zA-Z]+$/.test(input) ? true : 'Enter a valid color keyword or hexadecimal number.'
+    }
+  ]).then(answers => {
+    const { text, textColor, shape, shapeColor } = answers;
+    let selectedShape;
+
+    switch (shape) {
+      case 'circle':
+        selectedShape = new Circle(shapeColor);
+        break;
+      case 'square':
+        selectedShape = new Square(shapeColor);
+        break;
+      case 'triangle':
+        selectedShape = new Triangle(shapeColor);
+        break;
+    }
+
+    // Now we assemble the SVG without repeating the <svg> tag in each shape's render method
+    const svgContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${selectedShape.render()}
+      <text x="150" y="100" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-size="50" font-family="Arial">${text}</text>
+      </svg>`;
+
+    fs.writeFileSync('logo.svg', svgContent);
+    console.log('Generated logo.svg');
+  });
+}
+
+createLogo();
